aa7f17379909514e2caa225826d27c6c7b8b84cb
GegeZZZ <54515838+GegeZZZ@users.noreply.github.com>
2020-08-21 18:51:18 -0400

    Update end_of_year_video.js

--- src/end_of_year_video.js
+++ src/end_of_year_video.js
@@ -3,23 +3,23 @@
 const slackAction = require("./slack_action");
 
 const PROMPT_TO_RESPONSES = {
-    "Nice view!": "Yeah - Maine was awesome!",
-    "Btw Darby, what's your favorite food?": "Data :stuck_out_tongue:",
-    "It looks like your days weren't sparse at all...": "Yes! I was very busy. Class, homework, dates, friends...",
-    "Darby... :man-facepalming:": "Sorry, wrong file!",
-    "Darby, I heard you are graduating today!": "Yes. And can I tell you a secret?",
-    "Sure.": "I am going to miss all my classmates a lot.",
-    "Thank you, Darby!": "Wait...",
-    "What is it?": "This can't end yet...",
+    "nice view!": "Yeah - Maine was awesome!",
+    "btw darby, what's your favorite food?": "Data :stuck_out_tongue:",
+    "it looks like your days weren't sparse at all...": "Yes! I was very busy. Class, homework, dates, friends...",
+    "darby... :man-facepalming:": "Sorry, wrong file!",
+    "darby, i heard you are graduating today!": "Yes. And can I tell you a secret?",
+    "sure.": "I am going to miss all my classmates a lot.",
+    "thank you, darby!": "Wait...",
+    "what is it?": "This can't end yet...",
     "?": "Without a special performance brought to you by the MBAn class of 2020!!!"
 }
 
 const PROMPT_TO_VIDEOS = {
-    "Hey darby, how did it all start?": ["Let me show you.", "https://i.ibb.co/6tN7TL9/D1.png"],
-    "Seriously, what actual food do you like?": ["Here:", "https://i.ibb.co/gRQcJzq/D2.png"],
-    "Yummy... darby what else did you do all day other than eat?": [":tada:", "https://i.ibb.co/PMc79SW/D3.png"],
-    "It looks like your days weren’t sparse at all...": ["Look at my friends", "https://i.ibb.co/hC4h5Lb/D4.png"],
-    "Darby... :man-facepalming:": ["This should be better", "https://i.ibb.co/zQpsHn2/D5.png"],
+    "hey darby, how did it all start?": ["Let me show you.", "https://i.ibb.co/6tN7TL9/D1.png"],
+    "seriously, what actual food do you like?": ["Here:", "https://i.ibb.co/gRQcJzq/D2.png"],
+    "yummy... darby what else did you do all day other than eat?": [":tada:", "https://i.ibb.co/PMc79SW/D3.png"],
+    "it looks like your days weren’t sparse at all...": ["Look at my friends", "https://i.ibb.co/hC4h5Lb/D4.png"],
+    "darby... :man-facepalming:": ["This should be better", "https://i.ibb.co/zQpsHn2/D5.png"],
     "?": ["I will miss you", "https://i.ibb.co/c3HWJjT/D6.png"]
 }
 
