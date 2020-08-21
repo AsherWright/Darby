e73495b4a9b5e4e51900f99b3af671aa0cbb58d5
AsherWright <sastreen@hotmail.com>
2020-08-21 13:51:54 -0300

    Improve text for end of year

--- src/end_of_year_video.js
+++ src/end_of_year_video.js
@@ -5,20 +5,22 @@ const slackAction = require("./slack_action");
 const PROMPT_TO_RESPONSES = {
     "nice view!": "Yeah - Maine was awesome!",
     "btw darby, what’s your favorite food?": "Data :stuck_out_tongue:",
+    "it looks like your days weren’t sparse at all...": "Yes! I was very busy. Class, homework, dates, friends...",
     "darby... :man-facepalming:": "Sorry, wrong file!",
     "darby, i heard you are graduating today!": "Yes. And can I tell you a secret?",
     "sure.": "I am going to miss all my classmates a lot.",
     "thank you, darby!": "Wait...",
-    "what is it?": "This can't end yet..."
+    "what is it?": "This can't end yet...",
+    "?": "Without a special performance brought to you by the MBAn class of 2020!!!"
 }
 
 const PROMPT_TO_VIDEOS = {
     "hey darby, how did it all start?": ["Let me show you.", "https://i.ibb.co/6tN7TL9/D1.png"],
     "seriously, what actual food do you like?": ["Here:", "https://i.ibb.co/gRQcJzq/D2.png"],
     "yummy... darby what else did you do all day other than eat?": [":tada:", "https://i.ibb.co/PMc79SW/D3.png"],
-    "it looks like your days weren’t sparse at all...": ["Yes! I was very busy. Class, homework, dates, friends...", "https://i.ibb.co/hC4h5Lb/D4.png"],
+    "it looks like your days weren’t sparse at all...": ["Look at my friends", "https://i.ibb.co/hC4h5Lb/D4.png"],
     "darby... :man-facepalming:": ["This should be better", "https://i.ibb.co/zQpsHn2/D5.png"],
-    "?": ["Without a special performance brought to you by the MBAn class of 2020!!!", "https://i.ibb.co/c3HWJjT/D6.png"]
+    "?": ["I will miss you", "https://i.ibb.co/c3HWJjT/D6.png"]
 }
 
 function respondToEndOfYearVideo(text, channel) {
